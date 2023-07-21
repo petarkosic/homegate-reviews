b17d77713e71beed46b6a60a2cc96969a539b369
petarkosic <avt3914@gmail.com>
2023-07-21 23:45:29 +0200

    fix: new database schema

--- server/db/database.sql
+++ server/db/database.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE homegate_reviews;
 
--- users
 CREATE TABLE users(
     user_id SERIAL,
     first_name VARCHAR(255) NOT NULL,
@@ -11,14 +10,26 @@ CREATE TABLE users(
     PRIMARY KEY (user_id)
 );
 
--- reviews
+CREATE TABLE apartments(
+    apartment_id SERIAL,
+    cost_of_rent DECIMAL,
+    number_of_rooms INTEGER,
+    living_space_sqm DECIMAL,
+    address TEXT,
+    availability TEXT,
+    description TEXT,
+    features_and_furnishings TEXT[],
+    PRIMARY KEY (apartment_id)
+);
+
 CREATE TABLE reviews(
     review_id SERIAL,
-    user_id SERIAL,
+    user_id INTEGER,
     apartment_id INTEGER,
     review_text TEXT NOT NULL,
     date_of_review TIMESTAMP NOT NULL DEFAULT NOW(),
     star_rating INTEGER NOT NULL,
     PRIMARY KEY (review_id),
-    FOREIGN KEY (user_id) REFERENCES users (user_Id)
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (apartment_id) REFERENCES apartments (apartment_id)
 );
