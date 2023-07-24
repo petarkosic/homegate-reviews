37662c9971af75890c57ce3ad1472808b2972a20
petarkosic <avt3914@gmail.com>
2023-07-24 23:22:48 +0200

    add: apartment db query fix: database column type

--- server/controllers/apartmentController.ts
+++ server/controllers/apartmentController.ts
@@ -2,19 +2,55 @@ import { Request, Response } from 'express';
 import pool from './../db/db';
 
 const getApartment = async (req: Request, res: Response) => {
+	const client = await pool.connect();
+
 	try {
+		await client.query('BEGIN');
+
 		const { id } = req.params;
-		const apartment = await pool.query(
-			'SELECT * FROM apartments WHERE apartment_id = $1',
-			[id]
-		);
+
+		const queryString = `
+		SELECT
+			a.apartment_id,
+			a.cost_of_rent,
+			a.number_of_rooms,
+			a.living_space_sqm,
+			a.address,
+			a.availability,
+			a.description,
+			a.features_and_furnishings,
+			r.review_id,
+			r.review_text,
+			r.date_of_review,
+			r.star_rating,
+			u.user_id AS reviewer_id,
+			u.first_name AS reviewer_first_name,
+			u.last_name AS reviewer_last_name,
+			u.email AS reviewer_email
+		FROM
+			apartments AS a
+		LEFT JOIN
+			reviews AS r ON a.apartment_id = r.apartment_id
+		LEFT JOIN
+			users AS u ON r.user_id = u.user_id
+		WHERE
+			a.apartment_id = $1;
+		`;
+
+		const apartmentQuery = await client.query(queryString, [id]);
+
+		await client.query('COMMIT');
+
 		res.json({
 			message: 'success',
-			data: apartment.rows[0],
+			data: apartmentQuery.rows[0] || [],
 		});
 	} catch (err) {
+		await client.query('ROLLBACK');
 		console.error(err);
 		res.status(500).json('Server error');
+	} finally {
+		client.release();
 	}
 };
 
--- server/db/database.sql
+++ server/db/database.sql
@@ -15,8 +15,8 @@ CREATE TABLE apartments(
     cost_of_rent DECIMAL,
     number_of_rooms INTEGER,
     living_space_sqm DECIMAL,
-    address TEXT,
-    availability TEXT,
+    address VARCHAR(255),
+    availability VARCHAR(20),
     description TEXT,
     features_and_furnishings TEXT[],
     PRIMARY KEY (apartment_id)
@@ -28,7 +28,7 @@ CREATE TABLE reviews(
     apartment_id INTEGER,
     review_text TEXT NOT NULL,
     date_of_review TIMESTAMP NOT NULL DEFAULT NOW(),
-    star_rating INTEGER NOT NULL,
+    star_rating INTEGER NOT NULL CHECK (star_rating BETWEEN 1 AND 5),
     PRIMARY KEY (review_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id),
     FOREIGN KEY (apartment_id) REFERENCES apartments (apartment_id)
