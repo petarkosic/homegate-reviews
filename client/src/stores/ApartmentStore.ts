d2c7a8b300c3240c4b668e9a9247fb43c976dbc4
petarkosic <avt3914@gmail.com>
2023-08-04 23:48:17 +0200

    add: store apartment and review types add: grid container layout

--- client/src/stores/ApartmentStore.ts
+++ client/src/stores/ApartmentStore.ts
@@ -1,7 +1,40 @@
-import { defineStore } from 'pinia';
+import { StoreDefinition, defineStore } from 'pinia';
 
-export const useApartmentStore = defineStore('apartment', {
-	state: () => ({
+export interface Apartment {
+	apartment_id: string;
+	cost_of_rent: number;
+	number_of_rooms: number;
+	living_space_sqm: number;
+	address: string;
+	availability: string;
+	description: string;
+	features_and_furnishings: string[] | [];
+	reviews?: Review[];
+}
+
+export interface Review {
+	review_id: number;
+	review_text: string;
+	date_of_review: string;
+	star_rating: number;
+	reviewer_id: number;
+	reviewer_first_name: string;
+	reviewer_last_name: string;
+	reviewer_email: string;
+	reviewer_registered_at: string;
+}
+
+export const useApartmentStore: StoreDefinition<
+	'apartment',
+	{
+		apartment: Apartment | null;
+	},
+	{},
+	{
+		getApartment(id: string): Promise<void>;
+	}
+> = defineStore('apartment', {
+	state: (): { apartment: Apartment | null } => ({
 		apartment: null,
 	}),
 	actions: {
--- client/src/views/ApartmentView.vue
+++ client/src/views/ApartmentView.vue
@@ -5,7 +5,7 @@ import Navbar from '../components/Navbar.vue';
 
 const apartmentStore = useApartmentStore();
 
-const apartment = computed(() => apartmentStore.apartment);
+const apartment: any = computed(() => apartmentStore.apartment);
 </script>
 
 <template>
@@ -13,14 +13,40 @@ const apartment = computed(() => apartmentStore.apartment);
 		<main>
 			<Navbar />
 		</main>
-		<div>
-			<div v-if="apartment" class="wrapper">
-				<h2>Apartment Name: {{ apartment.name }}</h2>
-				<p>Apartment Description: {{ apartment.description }}</p>
+		<div class="grid-container">
+			<div v-if="apartment">
+				<div class="images">
+					<h1>images</h1>
+				</div>
+			</div>
+			<div v-if="apartment" class="apartment-details">
+				<h2>
+					{{ apartment?.[0]?.address }}
+				</h2>
+				<p>Rent: {{ apartment?.[0]?.cost_of_rent }} chf</p>
+				<p>Available: {{ apartment?.[0]?.availability }}</p>
+				<p>
+					Living space: {{ apartment?.[0]?.living_space_sqm }} m<sup>2</sup>
+				</p>
+				<p>Number of rooms: {{ apartment?.[0]?.number_of_rooms }}</p>
+				<p>
+					{{ JSON.parse(apartment?.[0].description) }} Apartment Description:
+				</p>
 				<!-- Add other apartment details as needed -->
 			</div>
-			<div v-else>
+			<div v-else class="no-apartment">
 				<p>No apartment details available.</p>
+			</div>
+			<div v-if="apartment">
+				<div class="reviews">
+					<h2>Reviews</h2>
+					<div v-for="(item, index) in apartment" :key="index" class="review">
+						<p>
+							{{ item?.reviewer_first_name }} {{ item?.reviewer_last_name }}
+						</p>
+						<p>{{ item?.review_text }}</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -34,5 +60,38 @@ section {
 
 main {
 	display: flex;
+}
+.grid-container {
+	display: grid;
+	grid-template-columns: 1.5fr 1fr;
+	grid-template-rows: 1.5fr 1fr;
+	gap: 2rem;
+	grid-template-areas:
+		'images apartment-details'
+		'reviews reviews';
+}
+
+.no-apartment {
+	grid-column: 1 / 3;
+}
+
+.images {
+	grid-area: images;
+}
+
+.apartment-details {
+	grid-area: apartment-details;
+	text-align: left;
+	padding: 20px;
+}
+
+.reviews {
+	grid-area: reviews;
+	text-align: left;
+	padding: 20px;
+}
+
+sup {
+	line-height: 1;
 }
 </style>
