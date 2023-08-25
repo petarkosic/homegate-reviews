a30aac8930fc464604da31c0296e755ecf82a3e0
petarkosic <avt3914@gmail.com>
2023-08-25 23:59:33 +0200

    add: homeview refactor add: review component add: navbar refactor

--- client/src/components/Reviews.vue
+++ client/src/components/Reviews.vue
@@ -1,18 +1,54 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useApartmentStore } from '../stores/ApartmentStore';
 import { formatDate } from './../helpers/helpers';
 import StarRating from './StarRating.vue';
+import LoginModal from './LoginModal.vue';
+import AddReviewModal from './AddReviewModal.vue';
+
+interface UserData {
+	user_id: number;
+	first_name: string;
+	last_name: string;
+	email: string;
+	registered_at: string;
+	access_token: string;
+	message: string;
+}
+
+const user = JSON.parse(localStorage.getItem('user') || 'null') as UserData;
+const isLoggedIn = ref<boolean>(user ? true : false);
+const shouldOpenModal = ref<boolean>(false);
 
 const apartmentStore = useApartmentStore();
 
 const apartment: any = computed(() => apartmentStore.apartment);
+
+const handleReviewModal = (): void => {
+	if (!user) {
+		shouldOpenModal.value = true;
+		isLoggedIn.value = false;
+	} else {
+		shouldOpenModal.value = true;
+		isLoggedIn.value = true;
+	}
+};
+
+const closeModals = (): void => {
+	isLoggedIn.value = false;
+	shouldOpenModal.value = false;
+};
 </script>
 
 <template>
-	<div v-if="apartment">
+	<div v-if="apartment" class="reviews-wrapper">
 		<div class="reviews">
-			<h2>Reviews</h2>
+			<div class="review-add">
+				<h2>Reviews</h2>
+				<button @click="handleReviewModal">
+					{{ user ? 'Add a Review' : 'Login to Review' }}
+				</button>
+			</div>
 			<div v-for="(item, index) in apartment" :key="index" class="review">
 				<div class="reviewer">
 					<p class="name">
@@ -26,6 +62,13 @@ const apartment: any = computed(() => apartmentStore.apartment);
 				</div>
 			</div>
 		</div>
+		<div v-if="shouldOpenModal" class="modals">
+			<LoginModal v-if="!isLoggedIn && shouldOpenModal" @close="closeModals" />
+			<AddReviewModal
+				v-else-if="isLoggedIn && shouldOpenModal"
+				@close="closeModals"
+			/>
+		</div>
 	</div>
 </template>
 
@@ -37,6 +80,16 @@ const apartment: any = computed(() => apartmentStore.apartment);
 	display: flex;
 	flex-direction: column;
 	gap: 1rem;
+}
+
+.review-add {
+	display: flex;
+	align-items: center;
+	gap: 4rem;
+}
+
+.review-add button {
+	padding: 1rem 2rem;
 }
 
 .review {
@@ -63,5 +116,18 @@ const apartment: any = computed(() => apartmentStore.apartment);
 
 .review-data sup {
 	line-height: 1;
+}
+
+.modals {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 100;
 }
 </style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,15 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useRouter } from 'vue-router';
 import Navbar from '../components/Navbar.vue';
-
-import { useApartmentStore } from './../stores/ApartmentStore';
-
-const router = useRouter();
-
-const apartmentStore = useApartmentStore();
-
-const apartment = computed(() => apartmentStore.apartment);
 </script>
 
 <template>
