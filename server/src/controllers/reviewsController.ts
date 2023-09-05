9ed8f3b1c5caacbdfe3eb9605ac6bf3d9a609e4a
petarkosic <avt3914@gmail.com>
2023-09-05 22:48:16 +0200

    fix: reviews controller invalid input check

--- server/src/controllers/reviewsController.ts
+++ server/src/controllers/reviewsController.ts
@@ -15,6 +15,12 @@ const leaveReview = async (req: Request, res: Response) => {
 		await client.query('BEGIN');
 		let { user_id, apartment_id, review_text, star_rating } = req.body;
 
+		if (!review_text || star_rating == null) {
+			return res.status(400).json({
+				message: 'Please enter all fields.',
+			});
+		}
+
 		review_text = `"${review_text}"`;
 
 		const queryString = `
